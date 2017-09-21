bad8e9bca7db39665e76373ab9806e71689b9c0f
Dmitry Peshkin <Dmitrey04@mail.ru>
2017-09-22 02:22:18 +0500

    blog made adaptive

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,17 +1,17 @@
 (function(){
-
     const navToggle = document.querySelector('.nav-toggle');
-    navToggle.addEventListener('click', function(e) {
-        e.preventDefault();
-        const btn = e.currentTarget;
-        const fsNav = document.querySelector('.fs-nav');
-        const fsNavActive = 'fs-nav_active';
-        const btnClose = 'nav-toggle__button_close';
+    if(navToggle)
+        navToggle.addEventListener('click', function(e) {
+            e.preventDefault();
+            const btn = e.currentTarget;
+            const fsNav = document.querySelector('.fs-nav');
+            const fsNavActive = 'fs-nav_active';
+            const btnClose = 'nav-toggle__button_close';
 		
-        fsNav.classList.toggle(fsNavActive);
-        btn.firstElementChild.classList.toggle(btnClose);
-        btn.parentElement.classList.toggle('header__menu-button_close');
-        document.body.style.overflow = (document.body.style.overflow === 'hidden' ? '' : 'hidden');
-    });
+            fsNav.classList.toggle(fsNavActive);
+            btn.firstElementChild.classList.toggle(btnClose);
+            btn.parentElement.classList.toggle('header__menu-button_close');
+            document.body.style.overflow = (document.body.style.overflow === 'hidden' ? '' : 'hidden');
+        });
 
 }());
