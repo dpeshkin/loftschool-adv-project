4e1225670c1c06db1230b3854b266de3b9d5b036
Dmitry Peshkin <Dmitrey04@mail.ru>
2017-09-27 21:23:49 +0500

    added login form

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,17 +1,34 @@
 (function(){
-    const navToggle = document.querySelector('.nav-toggle');
-    if(navToggle)
-        navToggle.addEventListener('click', function(e) {
-            e.preventDefault();
-            const btn = e.currentTarget;
-            const fsNav = document.querySelector('.fs-nav');
-            const fsNavActive = 'fs-nav_active';
-            const btnClose = 'nav-toggle__button_close';
-		
-            fsNav.classList.toggle(fsNavActive);
-            btn.firstElementChild.classList.toggle(btnClose);
-            btn.parentElement.classList.toggle('header__menu-button_close');
-            document.body.style.overflow = (document.body.style.overflow === 'hidden' ? '' : 'hidden');
+    // NAV-TOGGLE
+    function showNav (e){
+        e.preventDefault();
+        const btn = e.currentTarget;
+        document.querySelector('.fs-nav').classList.toggle('fs-nav_active');
+        btn.firstElementChild.classList.toggle('nav-toggle__button_close');
+        btn.parentElement.classList.toggle('header__menu-button_close');
+        document.body.style.overflow = (document.body.style.overflow === 'hidden' ? '' : 'hidden');
+    }
+
+    if(document.querySelector('.nav-toggle'))
+        document.querySelector('.nav-toggle').addEventListener('click', function(e) {
+            showNav(e);
         });
 
+    //FLIPPER-ROTATE
+    function flipperRotate(e) {
+        e.preventDefault();
+        document.querySelector('.flipper').classList.toggle('flipper_back');
+        document.querySelector('.btn_auth').classList.toggle('btn_auth-hidden');
+    }
+
+    if(document.querySelector('.btn_auth'))
+        document.querySelector('.btn_auth').addEventListener('click', function(e) {
+            flipperRotate(e);
+        });
+
+    if(document.getElementById('btn-main'))
+        document.getElementById('btn-main').addEventListener('click', function(e) {
+            flipperRotate(e);
+        });
 }());
+
