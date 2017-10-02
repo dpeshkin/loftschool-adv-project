98c7436b72781d6ccb697ffb64d2bf61a0aeb891
Dmitry Peshkin <Dmitrey04@mail.ru>
2017-10-03 02:55:34 +0500

    changed parallax

--- src/scripts/modules/parallax.js
+++ src/scripts/modules/parallax.js
@@ -2,15 +2,15 @@ var parallaxContainer = document.querySelector('.parallax'),
     layers = parallaxContainer.children;
 
 var moveLayers = function (e) {
-    var initialX = (window.innerWidth / 2) - e.pageX;
-    var initialY = (window.innerHeight / 2) - e.pageY;
+    var initialX = -e.pageX;
+    var initialY = -e.pageY;
 
     [].slice.call(layers).forEach(function (layer, index) {
         var
             divider = index / 100,
             positionX = initialX * divider,
             positionY = initialY * divider,
-            bottomPosition = (window.innerHeight / 2) * divider,
+            bottomPosition = (window.innerHeight) * divider,
             transformString = 'translate(' + positionX + 'px,' + positionY + 'px)',
             image = layer.firstElementChild;
 
