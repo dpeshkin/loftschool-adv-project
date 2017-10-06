6deeac46e03bffc107b597cd3fce1cd37a6dae43
Dmitry Peshkin <Dmitrey04@mail.ru>
2017-10-06 22:34:20 +0500

    preloader animated

--- src/scripts/modules/preloader.js
+++ src/scripts/modules/preloader.js
@@ -3,36 +3,46 @@ const preloader = (function () {
     const imagesCount = images.length;
     let imagesLoaded = 0;
     const preloader = document.querySelector('.preloader');
-    const loadPercent = preloader.querySelector('.loaded');
-    const imageLoaded = () => {
-        loadPercent.innerHTML = Math.round((100 / imagesCount) * imagesLoaded) + '%';
+    const preloaderText = preloader.querySelector('.loaded');
+    const preloaderCircle = preloader.querySelectorAll('.circle__preloader');
+    const preloaderIncrement = () => {
         imagesLoaded++;
+        let percent = Math.round((100 / imagesCount) * imagesLoaded);
+        preloaderText.innerHTML = percent + '%';
+        preloaderCircle.forEach((element) => {
+            let dasharrayLenght = element.getAttribute('r')*2*Math.PI;
+            setTimeout( () => {
+                element.style.strokeDasharray = percent/100*dasharrayLenght + ', ' + dasharrayLenght;
+            }, 1000);
+        });
         if(imagesLoaded >= imagesCount){
-            setTimeout(()=>{
+            setTimeout( () => {
                 preloader.style.opacity = '0';
-                setTimeout(()=>{
+                setTimeout( () => {
                     preloader.style.zIndex = '-1';
                 }, 1000);
             }, 1000);  
         }
     };
-
-    for( let i =0; i < imagesCount; i++ ){
-        let src = images[i].src;
-        var loadImage = function(url){
-            return new Promise((resolve, reject) => {
-                let img = new Image();
-                img.onload = resolve;
-                img.onerror = reject;
-                img.src = url;
-            });
-        };
-        const imgPromise = loadImage(src);
-        imgPromise.then(
-            ()=>imageLoaded(),
-            ()=>console.log('ERROR image '+src+' not loaded')
-        );
-    }
+    const imageLoaded = () =>{
+        for( let i =0; i < imagesCount; i++ ){
+            let src = images[i].src;
+            var loadImage = function(url){
+                return new Promise((resolve, reject) => {
+                    let img = new Image();
+                    img.onload = resolve;
+                    img.onerror = reject;
+                    img.src = url;
+                });
+            };
+            const imgPromise = loadImage(src);
+            imgPromise.then(
+                () => preloaderIncrement(),
+                () => console.log('ERROR image '+src+' not loaded')
+            );
+        }
+    };
+    
 
     return {
         init: imageLoaded,
