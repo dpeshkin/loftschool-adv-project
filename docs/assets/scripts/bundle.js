78ed08d359bc6d62f61a50c1caf5bbc0f13b230b
Dmitry Peshkin <Dmitrey04@mail.ru>
2017-10-14 13:40:20 +0500

    some fixes

--- docs/assets/scripts/bundle.js
+++ docs/assets/scripts/bundle.js
@@ -396,14 +396,23 @@ module.exports = preloader.init;
 /* 9 */
 /***/ (function(module, exports) {
 
-const formActions = function(){
+const formActions = (() => {
     const form = document.querySelector('form');
-    const formFields = form.querySelectorAll('.form__input');
-    const formCheckboxes = form.querySelectorAll('.form__c');
-    const wrongRadio = document.getElementById('robot-radio-2');
-    const formAction = form.getAttribute('action');
-    const formMethod = form.getAttribute('method');
-    const notification = document.querySelector('.notification');
+
+    // выводим собщение об успешной отправке формы
+    const notificationInit = () => { 
+        const notification = document.querySelector('.notification');
+        if(notification){
+            notification.classList.add('notification_visible');
+            const closeNotification = notification.querySelector('.btn');
+            closeNotification.addEventListener('click', (e) => {
+                e.preventDefault();
+                notification.classList.remove('notification_visible');
+                form.reset();
+            });
+        }
+    };
+    // проверяем инпуты формы
     const checkInput = (input) => {
         if (input.value.trim() === ''){
             input.classList.add('form__input_empty');
@@ -411,56 +420,76 @@ const formActions = function(){
             input.classList.remove('form__input_empty');
         }
     };
-    const checkBox = (input) => {
+    // проверяем чекбоксы
+    const checkBox = (input, wrongRadio) => {
         if (!input.checked || wrongRadio.checked){
             input.classList.add('form__c_empty');
         }else{
             input.classList.remove('form__c_empty');
         }
     };
-    wrongRadio.onclick = () => {
-        [].forEach.call(formCheckboxes, checkBox);
-    };
-    [].forEach.call(formFields, function(element){
-        element.addEventListener('blur', function(){
-            checkInput(element);
-        });
-    });
-    formCheckboxes.forEach(function(element){
-        element.addEventListener('click', function(){
-            checkBox(element);
-        });
-    });
 
-    if(notification){
-        const closeNotification = notification.querySelector('.btn');
-        closeNotification.addEventListener('click', (e) => {
-            e.preventDefault();
-            notification.classList.remove('notification_visible');
-            form.reset();
-        });
-    }
-
-    form.addEventListener('submit', (e) =>{
-        e.preventDefault();
-        [].forEach.call(formFields, checkInput);
-        [].forEach.call(formCheckboxes, checkBox);
-        if (!document.querySelector('.form__input_empty') && !document.querySelector('.form__c_empty')){
-            const formData = new FormData(form);
-            const xhr = new XMLHttpRequest();
-            xhr.open(formMethod, formAction);
-            xhr.send(formData);
-            if(notification) notification.classList.add('notification_visible');
+    // прослушка чекбоксов по click
+    const checkboxAddListeners = (formCheckboxes, wrongRadio) => {
+        if(formCheckboxes && wrongRadio) {
+            wrongRadio.onclick = () => {
+                formCheckboxes.forEach(checkBox);
+            };
+            formCheckboxes.forEach((element) => {
+                element.addEventListener('click', () => {
+                    formCheckboxes.forEach(checkBox);
+                });
+            });
         }
-    });
-};
+    };
 
-const formActionsInit = () => {
-    if(document.querySelector('form'))
-        formActions();
-};
+    // прослушка инпутов по onblur
+    const inputAddListeners = (formFields) => {
+        formFields.forEach((element) => {
+            element.addEventListener('blur', function(){
+                checkInput(element);
+            });
+        });
+    };
 
-module.exports = formActionsInit;
+    // проверка формы при сабмите
+    const formSubmit = (formFields, formCheckboxes) => {
+        form.addEventListener('submit', (e) =>{
+            e.preventDefault();
+            const formAction = form.getAttribute('action');
+            const formMethod = form.getAttribute('method');
+            formFields.forEach(checkInput);
+            formCheckboxes.forEach(checkBox);
+            if (!document.querySelector('.form__input_empty') && !document.querySelector('.form__c_empty')){
+                const formData = new FormData(form);
+                const xhr = new XMLHttpRequest();
+                xhr.open(formMethod, formAction);
+                xhr.send(formData);
+                notificationInit();
+            }
+        });
+    };
+
+    // инииализация модуля
+    const formActionsInit = () => {
+        if(form){
+            const formFields = form.querySelectorAll('.form__input');
+            const formCheckboxes = form.querySelectorAll('.form__c'); 
+            const wrongRadio = document.getElementById('robot-radio-2');
+            formSubmit(formFields, formCheckboxes);
+            inputAddListeners(formFields);
+            checkboxAddListeners(formCheckboxes, wrongRadio);
+        }
+    };
+
+    return {
+        init:formActionsInit,
+    };
+})();
+
+
+
+module.exports = formActions.init;
 
 /***/ }),
 /* 10 */
